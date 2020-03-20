a5a59d6f49397f98d48e208f8dc19b9dcac6af1b
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2020-03-20 13:43:24 +0100

    Add Tactic Notations at level higher than the dummy tactics

--- theories/Ltac1/Tactics.v
+++ theories/Ltac1/Tactics.v
@@ -1,3 +1,19 @@
 Declare ML Module "ltac_plugin".
 Declare ML Module "tactician_ltac1_record_plugin".
 Declare ML Module "tactician_ltac1_tactics_plugin".
+
+From Ltac2 Require Import Control Notations.
+From Tactician Require Import Ltac1Dummy.
+
+Tactic Notation (at level 5) "search" := ml_search.
+
+Tactic Notation (at level 5) "suggest" := ml_suggest.
+
+Ltac2 reprover tacs := prove_with tacs ltac1:(idtac
+    "A previously synthesized proof script by Tactician has failed."
+    "An attempt is being made to synthesize a new script.";
+    ml_search).
+
+Ltac2 Notation "[#" tacs(list0(thunk(self), "#")) "#]" : 3 := reprover tacs.
+
+Tactic Notation (at level 5) "search" "failing" tactic(t) := t.
