6b0d3a549ff9a22a5c34cf8a61e0774192cf50f9
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2020-09-08 21:09:15 +0200

    Lower tactic precedences (works better with try).

--- theories/Ltac1/Tactics.v
+++ theories/Ltac1/Tactics.v
@@ -6,6 +6,6 @@ Ltac searcher := idtac
     "A previously synthesized proof script by Tactician has failed."
     "An attempt is being made to synthesize a new script.";
     ml_search.
-Tactic Notation (at level 5) "search" "failing" tactic(t) := solve [t] || searcher.
-Tactic Notation (at level 5) "search" := ml_search.
-Tactic Notation (at level 5) "suggest" := ml_suggest.
+Tactic Notation (at level 2) "search" "failing" tactic(t) := solve [t] || searcher.
+Tactic Notation (at level 2) "search" := ml_search.
+Tactic Notation (at level 2) "suggest" := ml_suggest.
