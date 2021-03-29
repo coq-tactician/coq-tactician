3d326a3493cc776812fe8e0048b55279bc250ea7
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2021-03-29 09:01:27 +0200

    Tree count 8

--- src/lshf_learner.ml
+++ src/lshf_learner.ml
@@ -84,11 +84,11 @@ let declare_option name var d =
         | Some i -> var := i)
   }
 
-let depth_default = 15
+let depth_default = 9
 let depth = ref depth_default
 let () = declare_option ["Tactician"; "LSHF"; "Depth"] depth depth_default
 
-let trie_count_default = 11
+let trie_count_default = 8
 let trie_count = ref trie_count_default
 let () = declare_option ["Tactician"; "LSHF"; "Trie"; "Count"] trie_count trie_count_default
 
