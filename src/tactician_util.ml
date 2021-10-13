0a6444eabc83413b75d1fbfbb00e6a0c13e839b1
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2021-10-13 17:52:29 +0200

    Add proof state printer utility

--- src/tactician_util.ml
+++ src/tactician_util.ml
@@ -118,3 +118,31 @@ module ReaderStateMonad
   let local f x = fun r s -> x (f r) s
 end
 
+let proof_state_to_string hyps goal env evar_map =
+  let constr_str t = Sexpr.format_oneline (Printer.pr_econstr_env env evar_map t) in
+  let goal = constr_str goal in
+  let hyps = List.map (function
+      | Context.Named.Declaration.LocalAssum (id, typ) ->
+        let id_str = Names.Id.print id.binder_name in
+        let typ_str = constr_str typ in
+        Pp.(id_str ++ str " : " ++ typ_str)
+      | Context.Named.Declaration.LocalDef (id, term, typ) ->
+        let id_str = Names.Id.print id.binder_name in
+        let term_str = Pp.(str " := " ++ constr_str term) in
+        let typ_str = constr_str typ in
+        Pp.(id_str ++ term_str ++ str " : " ++ typ_str)
+    ) hyps in
+  Pp.(prlist_with_sep (fun () -> str ", ") (fun x -> x) hyps ++ str " |- " ++ goal)
+
+let pr_proof_tac () =
+  let open Proofview in
+  let open Notations in
+  Goal.goals >>= Ltacrecord.record_map (fun x -> x) >>= fun gls ->
+  let gls_string = List.map (fun gl ->
+      let env = Proofview.Goal.env gl in
+      let sigma = Proofview.Goal.sigma gl in
+      let hyps = Goal.hyps gl in
+      let goal = Goal.concl gl in
+      proof_state_to_string hyps goal env sigma)
+      gls in
+  List.iter Feedback.msg_notice gls_string; tclUNIT ()
