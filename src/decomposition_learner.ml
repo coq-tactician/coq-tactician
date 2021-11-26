9738c0aa31aa7806482b88b93f101d388a09b542
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2021-11-26 05:07:02 +0100

    Modifications to decomposition learner

--- src/decomposition_learner.ml
+++ src/decomposition_learner.ml
@@ -1,5 +1,6 @@
 open Tactic_learner
 open Tactician_util
+open Ltac_plugin
 
 let data_file =
   let file = ref None in
@@ -18,36 +19,60 @@ module DecompositionLearner : TacticianOnlineLearnerType = functor (TS : Tactici
   open TS
   module Learner = Lshf_learner.ComplexLSHF
 
-  type model = unit
+  type model = tactic list
+  let last_model : tactic list ref = Summary.ref ~name:"dataset-generator-learner-lastmodel" []
 
-  let empty () = ()
+  let empty () = []
 
   let cache_type name =
     let dirp = Global.current_dirpath () in
     if Libnames.is_dirpath_prefix_of dirp (Names.ModPath.dp @@ Names.Constant.modpath name) then `File else `Dependency
 
   let learn db status name outcomes tac =
-    match cache_type name with
-    | `File ->
-      let tac = tactic_repr tac in
-      let tac = Cata.glob_tactic_expr_cata Cata.default_sequence_record tac in
-      let strict_tac = Tactic_normalize.tactic_strict tac in
-      let free = Tactic_substitute.tactic_free_variables @@ strict_tac in
-      List.iter (fun outcome ->
-          let ps_hyps = proof_state_hypotheses outcome.before in
-          let bound_hyps = Context.Named.to_vars ps_hyps in
-          let free = List.filter (fun id -> not @@ Names.Id.Set.mem id bound_hyps) free in
-          (* if free != [] then *)
-          (*   begin *)
-          (*     Feedback.msg_warning (Pp.seq (List.map Names.Id.print free)); *)
-          (*     Feedback.msg_warning (Pptactic.pr_glob_tactic (Global.env ()) tac) *)
-          (*   end; *)
-          ()) outcomes
-      (* Feedback.msg_info @@ Pptactic.pr_glob_tactic (Global.env ()) @@ tactic_repr tac *)
-    | `Dependency -> ()
+    let outcomes = List.map (fun _ -> tac) outcomes in
+    let db = match cache_type name with
+    | `File -> outcomes @ db
+    | `Dependency -> db in
+    last_model := db;
+    db
 
   let predict db situations = IStream.empty
   let evaluate db _ _ = 0., db
+
+  (* We have to do some reversals before the evaluation *)
+  let preprocess model =
+    List.rev model
+
+  let generate_step tac =
+      (* let tac = tactic_repr tac in *)
+      (* let tac = Cata.glob_tactic_expr_cata Cata.default_sequence_record tac in *)
+      (* let strict_tac = Tactic_normalize.tactic_strict tac in *)
+      (* let free = Tactic_substitute.tactic_free_variables @@ strict_tac in *)
+      (* List.iter (fun outcome -> *)
+      (*     let ps_hyps = proof_state_hypotheses outcome.before in *)
+      (*     let bound_hyps = Context.Named.to_vars ps_hyps in *)
+      (*     let free = List.filter (fun id -> not @@ Names.Id.Set.mem id bound_hyps) free in *)
+      (*     if free != [] then *)
+      (*       begin *)
+      (*         Feedback.msg_warning (Pp.seq (List.map Names.Id.print free)); *)
+      (*         Feedback.msg_warning (Pptactic.pr_glob_tactic (Global.env ()) tac) *)
+      (*       end; *)
+    (*     ()) outcomes *)
+    let tac = tactic_repr tac in
+    let tac = Tactic_normalize.tactic_normalize tac in
+    let tac = Tactic_normalize.tactic_strict tac in
+    let tac = Extreme_tactic_normalize.tactic_normalize tac in
+    let tachash = string_of_int @@ Hashtbl.hash_param 255 255 tac in
+    let tacstr = Pp.string_of_ppcmds @@
+      Sexpr.format_oneline @@ Pptactic.pr_glob_tactic (Global.env ()) @@ tac in
+    let tacsexpr = Sexpr.sexpr_to_string @@ Tactic_sexpr.tactic_sexpr @@ tac in
+    output_string (data_file ()) (tachash ^ "\t" ^ tacstr ^ "\t" ^ tacsexpr ^ "\n")
+
+  let endline_hook () = print_endline "writing";
+    let data = preprocess !last_model in
+    ignore (List.iter generate_step data)
+
+  let () = Declaremods.append_end_library_hook endline_hook
 
 end
 
