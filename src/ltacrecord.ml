75a83e763f481b2158e3fd77f4f0df13533d54b0
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-02-06 17:39:09 +0100

    Reorder functions to allow enabling memory

--- src/ltacrecord.ml
+++ src/ltacrecord.ml
@@ -329,19 +329,6 @@ type goal_stack = Proofview.Goal.t list list
 type tactic_trace = glob_tactic_expr list
 type state_id_stack = int list
 
-let mk_outcome (st, sts) =
-  (* let mem = (List.map TS.tactic_make (get_tactic_trace st)) in *)
-  let st : proof_state = goal_to_proof_state st in
-  { parents = [] (* List.map (fun tac -> (st (\* TODO: Fix *\), { executions = []; tactic = tac })) mem *)
-  ; siblings = End
-  ; before = st
-  ; after = [] (* List.map goal_to_proof_state sts *) }
-
-let mk_data_in outcomes tactic name path =
-  let tactic = TS.tactic_make tactic in
-  let outcomes = List.map mk_outcome outcomes in
-  { outcomes; tactic; name; status = Original; path }
-
 let record_field : bool Evd.Store.field = Evd.Store.field ()
 let name_field : (Names.Constant.t * Libnames.full_path) Evd.Store.field = Evd.Store.field ()
 let localdb_field : localdb Evd.Store.field = Evd.Store.field ()
@@ -464,6 +451,19 @@ let push_tactic_trace tac =
 
 let get_tactic_trace gl =
   get_field_goal2 tactic_trace_field gl (fun _ -> [])
+
+let mk_outcome (st, sts) =
+  (* let mem = (List.map TS.tactic_make (get_tactic_trace st)) in *)
+  let st : proof_state = goal_to_proof_state st in
+  { parents = [] (* List.map (fun tac -> (st (\* TODO: Fix *\), { executions = []; tactic = tac })) mem *)
+  ; siblings = End
+  ; before = st
+  ; after = [] (* List.map goal_to_proof_state sts *) }
+
+let mk_data_in outcomes tactic name path =
+  let tactic = TS.tactic_make tactic in
+  let outcomes = List.map mk_outcome outcomes in
+  { outcomes; tactic; name; status = Original; path }
 
 let add_to_db2 id ((outcomes, tactic) : (Proofview.Goal.t * Proofview.Goal.t list) list * glob_tactic_expr)
     sideff name path =
