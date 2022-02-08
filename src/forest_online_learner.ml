881ca4e3834225d0ccaa475addb7c470dd21bf2c
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2022-02-08 15:55:03 +0100

    100 0.3

--- src/forest_online_learner.ml
+++ src/forest_online_learner.ml
@@ -21,8 +21,8 @@ module OnlineForest : TacticianOnlineLearnerType = functor (TS : TacticianStruct
       let feats = proof_state_to_complex_ints_no_kind b in
       Forest.add
       ~min_impur:0.5
-      ~n_trees:3000
-      ~part:0.1
+      ~n_trees:100
+      ~part:0.3
       forest (Data.labeled (feats, obj))
 
     let learn db _loc outcomes tac =
