62af8e68fd3f7382f6d63a1b6c1d2b3d8a464c13
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-05-31 07:39:40 +0200

    400 0.3

--- src/forest_online_learner.ml
+++ src/forest_online_learner.ml
@@ -21,7 +21,7 @@ module OnlineForest : TacticianOnlineLearnerType = functor (TS : TacticianStruct
       let feats = proof_state_to_complex_ints_no_kind b in
       Forest.add
       ~min_impur:0.5
-      ~n_trees:200
+      ~n_trees:400
       ~part:0.3
       forest (Data.labeled (feats, obj))
 
