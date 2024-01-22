7926b9a5b7fabcec69b50aef39c8c73036f7a03a
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2024-01-22 11:11:11 +0100

    Bench lshf

--- src/lshf_learner.ml
+++ src/lshf_learner.ml
@@ -169,4 +169,4 @@ module ComplexLSHF : TacticianOnlineLearnerType =
   end
 
 (* let () = register_online_learner "SimpleLSHF" (module SimpleLSHF) *)
-(* let () = register_online_learner "ComplexLSHF" (module ComplexLSHF) *)
+let () = register_online_learner "ComplexLSHF" (module ComplexLSHF)
