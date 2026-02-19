13171f737c623aca738707573b3980d2cfea4533
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2026-02-19 17:23:32 +0100

    Adapt to rocq-prover/rocq#21655 (genarg interp must be a 'top type)

--- src/tactician_util.ml
+++ src/tactician_util.ml
@@ -149,17 +149,12 @@ open Util
 open Ltac_plugin
 open Tacexpr
 let val_tag wit = val_tag (Genarg.topwit wit)
-let register_interp0 wit f =
-  let open Ftactic.Notations in
-  let interp ist v =
-    f ist v >>= fun v -> Ftactic.return (Val.inject (val_tag wit) v)
-  in
-  Tacinterp.Register.register_interp0 wit interp
+let register_interp0 = Tacinterp.Register.register_interp0
 
 let wit_glbtactic : (Empty.t, glob_tactic_expr, glob_tactic_expr) Genarg.genarg_type =
   let wit = Genarg.create_arg "glbtactic" in
   let () = register_val0 wit None in
-  register_interp0 wit (fun ist v -> Ftactic.return v);
+  Tacinterp.Register.register_interp0 wit (fun ist v -> Ftactic.return v);
   wit
 
 let register tac name =
